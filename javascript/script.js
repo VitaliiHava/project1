d2e9ebdf07b2e5ad31cc1e3a4d97673f27b75b21
Viacheslav <vgava86@gmail.com>
2019-02-18 17:12:02 +0200

    form, handler, requests

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,46 @@
 $(document).ready(
-  //console.log("QOO QOO!")
-
+  console.log("Document Ready")
 );
 
-function onClick () {
-    alert ("QOO QOO!");
+
+$(document).ready(function showTable() {
+    $.ajax({
+        method: "POST",
+        url: "requests.php",
+        data: { request: "table" }
+    })
+        .done(function( response ) {
+            console.log (response);
+            $('#table').html(response);
+        });
+});
+
+function  income() {
+    var inc = $("#inputIncome").val();
+    //console.log("Доход: "+val);
+    $.ajax({
+        method: "POST",
+        url: "requests.php",
+        data: { income: inc }
+    })
+        .done(function( response ) {
+            console.log (response);
+            $('#inputIncome').val('');
+            $('#tableIncome').html(response);
+        });
+}
+
+function expense() {
+    znak = $("#inputExpense").val();
+    //console.log ("Расход: "+val);
+    $.ajax({
+        method: "POST",
+        url: "requests.php",
+        data: { expense: znak }
+    })
+        .done(function( response ) {
+            console.log (response);
+            $('#inputExpense').val('');
+            $('#tableIncome').html(response);
+        });
 }
--- requests.php
+++ requests.php
@@ -6,3 +6,22 @@
  * Time: 15:17
  */
 
+$pMysqli = new mysqli('127.0.0.1', 'root','','budget');
+
+$query = "SELECT * FROM debet";
+
+if (isset ($_POST['income'])) {
+    $inc = $_POST['income'];
+    if (filter_var($inc, FILTER_VALIDATE_INT)) {
+        echo $inc;
+    }
+    else die ("в поле ДОЛЖНО БЫТЬ ЧИСЛОВОЕ ЗНАЧЕНИЕ!");
+}
+
+if (isset ($_POST['expense'])) {
+    $exp = $_POST['expense'];
+    if (filter_var($exp, FILTER_VALIDATE_INT)) {
+        echo "$exp";
+    }
+    else die ("ВВЕДИ ЧИСЛОВОЕ ЗНАЧЕНИЕ!");
+}
