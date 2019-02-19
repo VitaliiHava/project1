69eea8103d9ff6ac8e6298f8ee0f3c9e296b0cc2
Viacheslav <vgava86@gmail.com>
2019-02-19 13:00:38 +0200

    added field category, use objects income, expense

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,16 @@
 $(document).ready(
-  console.log("Document Ready")
-);
-
-
-$(document).ready(function showTable() {
-    $.ajax({
-        method: "POST",
-        url: "requests.php",
-        data: { request: "table" }
-    })
-        .done(function( response ) {
-            console.log (response);
-            $('#table').html(response);
-        });
-});
+        function showTable() {
+            $.ajax({
+                method: "POST",
+                url: "requests.php",
+                data: { request: "table" }
+            })
+            .done(function( response ) {
+                console.log (response);
+                $('#table').html(response);
+            });
+        }
+    );
 
 function  income() {
     var inc = $("#inputIncome").val();
--- requests.php
+++ requests.php
@@ -13,7 +13,8 @@ $query = "SELECT * FROM debet";
 if (isset ($_POST['income'])) {
     $inc = $_POST['income'];
     if (filter_var($inc, FILTER_VALIDATE_INT)) {
-        echo $inc;
+        $response = new Income;
+        $response->setIncome($_POST['income']);
     }
     else die ("ВВЕДИ ЧИСЛОВОЕ ЗНАЧЕНИЕ!");
 }
@@ -21,7 +22,24 @@ if (isset ($_POST['income'])) {
 if (isset ($_POST['expense'])) {
     $exp = $_POST['expense'];
     if (filter_var($exp, FILTER_VALIDATE_INT)) {
-        echo "$exp";
+        $response = new Expense;
+        $response->setExpense($_POST['expense']);
     }
     else die ("ВВЕДИ ЧИСЛОВОЕ ЗНАЧЕНИЕ!");
+}
+
+class Income {
+    public $inc;
+    public function setIncome ($inc) {
+        $this->$inc = $inc;
+        echo $inc;
+    }
+}
+
+class Expense {
+    public $exp;
+    public function setExpense ($exp) {
+        $this->$exp = $exp;
+        echo $exp;
+    }
 }
